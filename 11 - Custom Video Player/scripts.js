4c02d9e818901ccfdc475db6350d47576643fa51
Nicholas Heilman <Nicholas.heilman@gmail.com>
2019-05-15 16:43:48 -0500

    update icon button

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -14,7 +14,16 @@ function togglePlay(){
         video.pause();
     }
 }
+function updateButton(){
+    const icon = this.paused ? '►' : '❚ ❚';
+    toggle.textContent = icon;
+    console.log(icon);
+}
 
 //event listeners
 // video.addEventListener('click', togglePlay);
+video.addEventListener('play', updateButton);
+video.addEventListener('pause', updateButton);
+
+
 toggle.addEventListener('click', togglePlay);
