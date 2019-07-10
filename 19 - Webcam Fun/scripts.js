7e3aedb3cabfb2eb1078ec81958c45edac76d06d
Nicholas Heilman <Nicholas.heilman@gmail.com>
2019-07-10 09:30:40 -0500

    paint to canvas and photo download

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -4,27 +4,10 @@ const ctx = canvas.getContext('2d');
 const strip = document.querySelector('.strip');
 const snap = document.querySelector('.snap');
 
-
-// function getVideo(){
-//     navigator.mediaDevices.getUserMedia({ video: true, audio: false})
-//         .then(localMediaStream => {
-//             console.log(localMediaStream);
-//             video.src = window.URL.createObjectURL(localMediaStream);
-//             video.play();
-//     });
-// }
 function getVideo() {
     navigator.mediaDevices.getUserMedia({ video: true, audio: false })
       .then(localMediaStream => {
         console.log(localMediaStream);
-      
-  //  DEPRECIATION : 
-  //       The following has been depreceated by major browsers as of Chrome and Firefox.
-  //       video.src = window.URL.createObjectURL(localMediaStream);
-  //       Please refer to these:
-  //       Depreceated  - https://developer.mozilla.org/en-US/docs/Web/API/URL/createObjectURL
-  //       Newer Syntax - https://developer.mozilla.org/en-US/docs/Web/API/HTMLMediaElement/srcObject
-        
         video.srcObject = localMediaStream;
         video.play();
       })
@@ -33,4 +16,31 @@ function getVideo() {
       });
   }
 
-getVideo();
+  function paintCanvas(){
+      const width = video.videoWidth;
+      const height = video.videoHeight;
+    //   console.log( width, height);
+      canvas.width = width;
+      canvas.height = height;
+      setInterval(() => {
+         ctx.drawImage(video, 0, 0, width, height);
+      }, 16);
+  }
+
+  function takePhoto(){
+      // camera sound
+      snap.currentTime = 0;
+      snap.play();
+      // get data from canvas
+      const data = canvas.toDataURL('image/jpeg');
+      const link = document.createElement('a');
+      link.href = data;
+      link.setAttribute('download', 'photo');
+      link.innerHTML = `<img src="${data}" alt="Image Name" />`;
+      strip.insertBefore(link, strip.firstChild);
+
+  }
+
+
+// getVideo();
+video.addEventListener('canplay',paintCanvas);
