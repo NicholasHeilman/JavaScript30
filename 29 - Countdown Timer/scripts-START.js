6a92de0615be4ef5e0543e5ca15083f51ee08bae
Nicholas Heilman <Nicholas.heilman@gmail.com>
2019-10-30 14:39:59 -0500

    add display for timer

--- 29 - Countdown Timer/scripts-START.js
+++ 29 - Countdown Timer/scripts-START.js
@@ -1,9 +1,43 @@
+let countdown;
+const timerDisplay = document.querySelector('.display__time-left');
+const endTime = document.querySelector('.display__end-time');
+const buttons = document.querySelectorAll('[data-time]');
+
 function timer(seconds){
     const now = Date.now();
     const then = now + seconds * 1000;
+    displayTimeLeft(seconds);
+    displayEndTime(then);
 
-    setInterval(() => {
+    countdown = setInterval(() => {
         const secondsLeft = Math.round((then - Date.now())/ 1000);
-        console.log(secondsLeft);
+        if(secondsLeft < 0 ){
+            clearInterval(countdown);
+            return;
+        }
+        displayTimeLeft(secondsLeft);
     }, 1000); 
 }
+
+function displayTimeLeft(seconds){
+    const minutes = Math.floor(seconds / 60);
+    const remainderSeconds = seconds % 60;
+    const display = `${minutes}:${remainderSeconds < 10 ? '0' : '' }${remainderSeconds}`;
+    document.title = display;
+    timerDisplay.textContent = display;
+    // console.log({minutes, remainderSeconds});
+}
+
+ function displayEndTime(timestamp){
+    const end = new Date(timestamp);
+    const hour = end.getHours();
+    const adjustedHour = hour > 12 ? hour -12 : hour;
+    const minutes = end.getMinutes();
+    endTime.textContent = `Be Back At ${hour}:${minutes < 10 ? '0' : '' } which is ${adjustedHour}:${minutes < 10 ? '0' : '' }`;
+ }
+function startTimer(){
+    // console.log("button", this.dataset.time);
+    const seconds = parseInt(this.dataset.time);
+    timer(seconds);
+}
+ buttons.forEach(button => button.addEventListener('click', startTimer));
